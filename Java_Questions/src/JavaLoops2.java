1236c0a53903d9281bee5db05b7f2b2c9aceae50
Chanaka1215 <nuwan.c.fernando@gmail.com>
2017-07-04 16:42:45 +0530

    loop 2 finished

---
+++ Java_Questions/src/JavaLoops2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by Chanaka Fernando on 7/4/2017.
+ */
+public class JavaLoops2 {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            double result=a;
+            for(int loop=0;loop < n;loop++){
+                double power =Math.pow(2,loop);
+                result += b*power ;
+                System.out.print((int)result+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
